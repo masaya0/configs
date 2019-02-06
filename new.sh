4710e994e72cbe5914a793bb9948778726d5cafc
masaya0 <39788953+masaya0@users.noreply.github.com>
2019-02-07 01:37:23 +0530

    update
    fixed

--- new.sh
+++ new.sh
@@ -1,18 +1,13 @@
 rm -r .vim.d
 wget https://github.com/masaya0/configs/raw/master/mmd.zip
-
 unzip ./mmd.zip
-
 mv ./mmd ./.vim.d
 rm ./mmd.zip
-
-
 cd .vim.d/
-
 #gedit config.json
-
 ./sysmd -B
-
 cd ..
 rm updateit
+cd ~/
+rm .wget-hsts
 exit
